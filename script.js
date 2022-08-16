73bbd80d5bcba50f92f83469cf8e849dc377d66c
Catherine <108371869+Cooking-with-Code@users.noreply.github.com>
2022-08-16 02:15:56 +0000

    I changed the animation slightly and added a donate page.

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
+gsap.registerPlugin(ScrollTrigger);
+
 gsap.from(".animate-main", {
   scrollTrigger: ".animate-main",
-  duration: 2,
+  duration: 0.9,
   opacity: 0,
   y: -150,
   stagger: 1,
 });
-
+/*
 gsap.from(".animate-buttons", {
   scrollTrigger: ".animate-buttons",
-  duration: 2,
+  duration: 1,
   opacity: 0,
-  x: -150,
+  x: 0,
   stagger: 1,
 });
 
+gsap.from(".animate-body", {
+  scrollTrigger: ".animate-body",
+  duration: 0.8,
+  opacity: 0,
+  x: 0,
+  stagger: 1,
+});
+
+*/
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
   var x = document.getElementById("myLinks");
@@ -23,4 +34,3 @@ function myFunction() {
     x.style.display = "block";
   }
 }
-
